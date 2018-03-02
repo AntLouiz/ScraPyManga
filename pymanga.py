30c1c2e2334a5a51188017af2d0f933bfec0063d
AntLouiz <luizrodrigo46@hotmail.com>
2018-03-02 16:30:45 -0300

    module refactored

--- pymanga.py
+++ pymanga.py
@@ -7,12 +7,18 @@ from scrapy.crawler import CrawlerProcess
 from MangaFinder.spiders.reader import ReaderSpider
 
 class Pymanga:
-    arg_parser = Cli #set a Command Line Interface
-    
-    def __init__(self, *args, **kwargs):
-        self.args = self.arg_parser.parse_args() #get the parser args
-        self.title = self.args.name # get the value of manga title
-        self.chapter = self.args.chap # get the value of manga chapters 
+
+    def __init__(self, title=None, chapter=None):
+        if not isinstance(title, str):
+            msg = "title argument must be str."
+            raise ValueError(msg)
+
+        elif not isinstance(chapter, int):
+            msg = "chapter argument must be int."
+            raise ValueError(msg)
+
+        self.title = title
+        self.chapter = chapter
 
         self.manga_data = {
                 'title': self.title,
@@ -31,7 +37,10 @@ class Pymanga:
         self.crawler_proccess.start()
 
 def main():
-    pymanga = Pymanga()
+    arg_parser = Cli #set a Command Line Interface
+    args = arg_parser.parse_args() #get the parser args
+
+    pymanga = Pymanga(" ".join(args.name), args.chap[0])
     pymanga.run()
 
 if __name__ == "__main__":
